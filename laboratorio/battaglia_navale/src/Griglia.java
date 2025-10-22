6b42712c7572a1d44bba33c12144f13f92699526
ripamonti-daniele <ripamonti.daniele.08@itisdalmine.edu.it>
2025-10-22 13:55:23 +0200

    continuo battaglia navale

--- laboratorio/battaglia_navale/src/Griglia.java
+++ laboratorio/battaglia_navale/src/Griglia.java
@@ -1,3 +1,63 @@
-public class Griglia {
+import javax.print.attribute.standard.Finishings;
+import java.io.IO;
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+import java.util.Random;
 
+public class Griglia {
+    private int[][] caselle;
+    private Nave[] navi;
+    private int numNavi;
+    private final int ascisse;
+    private final int ordinate;
+
+    public Griglia(int numNavi) {
+        ascisse = 10;
+        ordinate = 10;
+        this.numNavi = numNavi;
+        navi = new Nave[this.numNavi];
+    }
+
+    private void creaNave(int indice, int lunghezza, boolean direzione, int[] coordinate_iniziali) throws InvalidParameterException {
+        if (direzione && coordinate_iniziali.length != 2 || coordinate_iniziali[0] > ordinate - lunghezza || !direzione && coordinate_iniziali.length != 2 || coordinate_iniziali[1] > ascisse - lunghezza) throw new InvalidParameterException("Coordinate iniziali non valide");
+        navi[indice] = new Nave(lunghezza, coordinate_iniziali, direzione);
+    }
+
+    //da finire
+    public void chiediAttributiNaviConsole() {
+        for(int i = 0; i < numNavi; i++) {
+            System.out.println("Inserisci la lunghezza della nave");
+            int lunghezza = Integer.parseInt(IO.readln());
+            System.out.println("Inserisci la coordinata x");
+            int x = Integer.parseInt(IO.readln());
+            System.out.println("Inserisci la coordinata y");
+            int y = Integer.parseInt(IO.readln());
+            int[] coordinate_iniziali = {y, x};
+            String scelta;
+            boolean direzione;
+            do {
+                System.out.println("Scegli la direzione (1 = orizzonale, 0 = vericale)");
+                scelta = IO.readln();
+            } while (!scelta.equals("0") && !scelta.equals("1"));
+            direzione = scelta.equals("1");
+
+            creaNave(i, lunghezza, direzione, coordinate_iniziali);
+        }
+    }
+
+    private void inizializzaCaselle() {
+        for (int i = 0; i < ordinate; i++) {
+            for(int j = 0; j < ascisse; j++) caselle[i][j] = 0;
+        }
+    }
+
+    public void inserisciNavi() {
+        for (int i = 0; i < ordinate; i++) {
+            for(int j = 0; j < ascisse; j++) {
+                for (Nave n : navi) {
+                    if (Arrays.asList(n.getAscisse()).contains(j) && Arrays.asList(n.getOrdinate()).contains(i)) caselle[i][j] = 1;
+                }
+            }
+        }
+    }
 }
--- laboratorio/battaglia_navale/src/Nave.java
+++ laboratorio/battaglia_navale/src/Nave.java
@@ -1,36 +1,57 @@
 import java.security.InvalidParameterException;
 
 public class Nave {
-    private int lunghezza;
-    private int[] coordinate_iniziali;
-    private int larghezza_griglia;
-    private int altezza_griglia;
-    private boolean direzione; //true = orizzontale, false = vericale
-    private int[][] coordinate;
+    private final int lunghezza;
+    private final boolean direzione; //true = orizzontale, false = vericale
+    private final int[] ascisse;
+    private final int[] ordinate;
+    private final int[] coordinate_iniziali;
+    private final boolean[] stato_nave;
 
-    public Nave(int lunghezza, int[] coordinate_iniziali, int altezza_griglia, int larghezza_griglia, boolean direzione) throws InvalidParameterException {
+    public Nave(int lunghezza, int[] coordinate_iniziali, boolean direzione) {
         if (lunghezza < 2 || lunghezza > 5) throw new InvalidParameterException("Lunghezza nave non valida");
         this.direzione = direzione;
         this.lunghezza = lunghezza;
+        this.coordinate_iniziali = coordinate_iniziali;
+        stato_nave = new boolean[this.lunghezza];
+
         if (direzione) {
-            if (coordinate_iniziali.length != 2 || coordinate_iniziali[0] > altezza_griglia - lunghezza) throw new InvalidParameterException("Coordinate iniziali non valide");
-            coordinate = new int[this.lunghezza][1];
+            ascisse = new int[lunghezza];
+            ordinate = new int[1];
+            ordinate[0] = coordinate_iniziali[1];
         }
+
         else {
-            if (coordinate_iniziali.length != 2 || coordinate_iniziali[1] > larghezza_griglia - lunghezza) throw new InvalidParameterException("Coordinate iniziali non valide");
-            coordinate = new int[1][this.lunghezza];
+            ordinate = new int[lunghezza];
+            ascisse = new int[1];
+            ascisse[0] = coordinate_iniziali[0];
+        }
+
+        inizializza();
+    }
+
+    private void inizializza() {
+        for (int i = 0; i < lunghezza; i++) {
+            stato_nave[i] = false;
+            if (direzione) ascisse[i] = i + coordinate_iniziali[0];
+            else ordinate[i] = i + coordinate_iniziali[1];
         }
     }
 
-    // da finire
-    public int[][] getCoordinate() {
-        if (direzione) {
-            coordinate = new int[1][lunghezza];
-            for(int i = 0; i < lunghezza; i++) {
-                coordinate[0][i] = coordinate_iniziali[1];
-            }
-        }
+    public int[] getAscisse() {
+        return ascisse.clone();
+    }
 
-        return coordinate;
+    public int[] getOrdinate() {
+        return ordinate.clone();
+    }
+
+    public boolean[] getStatoNave() {
+        return stato_nave.clone();
+    }
+
+    public void colpisciNave(int pos) {
+        if (stato_nave[pos]) throw new InvalidParameterException("Casella nave già colpita");
+        stato_nave[pos] = true;
     }
 }

33e710548e71a64d0c003f987628a4b7d553fcac
ripamonti-daniele <ripamonti.daniele.08@itisdalmine.edu.it>
2025-12-13 11:32:34 +0100

    mdofiche a scacchiera (da finire)

--- laboratorio/scacchi/src/Scacchiera.java
+++ laboratorio/scacchi/src/Scacchiera.java
@@ -43,6 +43,7 @@ public class Scacchiera {
 
     public void muoviPedina(Pedina pedina, int[] posizione) {
         if (posizione[0] < 0 || posizione[0] > 7 || posizione[1] < 0 || posizione[1] > DIMENSIONE - 1) throw new IllegalArgumentException("Non esiste questa posizione nella scacchiera");
+
         if (pedina instanceof Cavallo) {
             if (caselle[posizione[0]][posizione[1]].getColore() == pedina.getColore()) throw new IllegalArgumentException("Il cavallo non può essere messo in una casella dove si trova una pedina del suo stesso colore");
             else {
@@ -51,13 +52,72 @@ public class Scacchiera {
                 pedina.muovi(posizione);
             }
         }
+
         else if (pedina instanceof Pedone) {
+            int avanzamento = -1;
+            if (pedina.getColore() == Color.white) avanzamento = 1;
+            if (Math.abs(posizione[1] - pedina.getPosizione()[1]) == 1 && posizione[0] == pedina.getPosizione()[0] + avanzamento) {
+                //mossa normale
+                if (caselle[posizione[0]][posizione[1]] != null && caselle[posizione[0]][posizione[1]].getColore() != pedina.getColore()) {
+                    caselle[posizione[0]][posizione[1]] = null;
+                    pedina.muovi(posizione);
+                }
+                //en passant
+                else if (caselle[posizione[0]][posizione[1]] == null && caselle[posizione[0]][posizione[1] - avanzamento].getColore() != pedina.getColore() && caselle[posizione[0]][posizione[1] - avanzamento] instanceof Pedone && ((Pedone) caselle[posizione[0]][posizione[1] - avanzamento]).getEnpassant()) {
+                    caselle[posizione[0]][posizione[1] - avanzamento] = null;
+                    pedina.muovi(posizione);
+                }
+            }
+
+            //mossa normale
+            else if (mosseIntermedieValide(pedina, posizione, false, true, 0, avanzamento) && caselle[posizione[0]][posizione[1]] == null) {
+                caselle[pedina.getPosizione()[0]][pedina.getPosizione()[0]] = null;
+                pedina.muovi(posizione);
+            }
+
+            else throw new IllegalArgumentException("Mossa non valida");
+
             //promozione
-            //cattura
-//            if (posizione[1] - pedina.getPosizione()[1] == 1 && posizione[0] == pedina.getPosizione()[0] && caselle[posizione[0]][posizione[1]] != null && caselle[posizione[0]][posizione[1]].getColore() != pedina.getColore()) {
-//
-//            }
-            //enpassant
+            if (pedina.getColore() == Color.white && pedina.getPosizione()[0] == DIMENSIONE - 1 || pedina.getColore() == Color.black && pedina.getPosizione()[0] == 0) {
+                promuoviPedone(pedina);
+            }
         }
+
+        else if (pedina instanceof Torre) {
+            int avanzamento = 1;
+            if (posizione[0] == pedina.getPosizione()[0]) {
+                if (posizione[1] - pedina.getPosizione()[1] < 0) {
+                    avanzamento = -1;
+
+                }
+            }
+        }
+    }
+
+    private boolean mosseIntermedieValide(Pedina pedina, int[] posizione, boolean orizzontale, boolean verticale, int incrementoOrizzontale, int incrementoVerticale) {
+        if (verticale && orizzontale) {
+            int j = posizione[1] + incrementoOrizzontale;
+            for (int i = posizione[0] + incrementoVerticale; i < pedina.getPosizione()[0]; i += incrementoVerticale) {
+                if (caselle[i][j] != null) return false;
+                j += incrementoOrizzontale;
+            }
+        }
+
+        else if (verticale) {
+            for (int i = posizione[0] + incrementoVerticale; i < pedina.getPosizione()[0]; i += incrementoVerticale) {
+                if (caselle[i][posizione[1]] != null) return false;
+            }
+        }
+
+        else if (orizzontale) {
+            for (int i = posizione[1] + incrementoOrizzontale; i < pedina.getPosizione()[1]; i += incrementoOrizzontale) {
+                if (caselle[posizione[0]][i] != null) return false;
+            }
+        }
+        return true;
+    }
+
+    private void promuoviPedone(Pedina pedina) {
+
     }
 }

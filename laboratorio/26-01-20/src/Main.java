45fb83cd739d519ddbc68787c262919af2b3ab97
ripamonti-daniele <ripamonti.daniele.08@itisdalmine.edu.it>
2026-01-28 13:50:05 +0100

    continuato programma automobile

--- laboratorio/26-01-20/src/Main.java
+++ laboratorio/26-01-20/src/Main.java
@@ -25,7 +25,6 @@ int chiediNumero(String msg) {
 }
 
 boolean create(List<Automobile> magazzino, String marca, String modello, String targa, int km, Color colore, LocalDate data, JLabel mostraErrore) {
-//    <html>Prima riga<br>Seconda riga<br>Terza riga</html>
     String label = "<html>";
     boolean valida = true;
 
@@ -86,6 +85,77 @@ boolean create(List<Automobile> magazzino, String marca, String modello, String 
     return valida;
 }
 
+int read(List<Automobile> magazzino, String targa, JLabel mostraErrore) {
+    for(int i = 0; i < magazzino.size(); i++) {
+        if (magazzino.get(i).getTarga().equals(targa)) {
+            mostraErrore.setText("Targa trovata");
+            return i;
+        }
+    }
+    mostraErrore.setText("Targa non trovata");
+    return -1;
+}
+
+boolean update(List<Automobile> magazzino, String targaOld, String targaNew, ButtonGroup colori, Map<String, Color> ottieniColore, int km, JLabel mostraErrore) {
+    int ind = read(magazzino, targaOld, mostraErrore);
+    if (ind == -1) return false;
+    boolean valido = true;
+    Color c = magazzino.get(ind).getColore();
+
+    String label = "<html>";
+
+    try {
+        c = ottieniColore.get(colori.getSelection().getActionCommand());
+        Automobile.controllaColore(c);
+    }
+    catch (IllegalArgumentException e1) {
+        label += e1.getMessage() + "<br>";
+        valido = false;
+    }
+    catch (Exception e2) {
+        label += "Seleziona un colore<br>";
+    }
+
+    if (!targaNew.equals(targaOld)) {
+        try {
+            Automobile.controllaTarga(targaOld);
+        }
+        catch (Exception e) {
+            label += e.getMessage() + "<br>";
+            valido = false;
+        }
+    }
+
+    if (valido) {
+        try {
+            magazzino.get(ind).aumentaKm(km);
+        }
+        catch (Exception e) {
+            label += e.getMessage() + "<br>";
+            mostraErrore.setText(label);
+            return false;
+        }
+        mostraErrore.setText("Macchina modificata correttamente");
+        magazzino.get(ind).cambiaTarga(targaNew);
+        magazzino.get(ind).setColore(c);
+    }
+
+    mostraErrore.setText(label);
+    return valido;
+}
+
+boolean delete(List<Automobile> magazzino, String targa, JLabel mostraErrore) {
+    for(int i = 0; i < magazzino.size(); i++) {
+        if (magazzino.get(i).getTarga().equals(targa)) {
+            magazzino.remove(i);
+            mostraErrore.setText("Macchina rimossa correttamente");
+            return true;
+        }
+    }
+    mostraErrore.setText("Targa non trovata");
+    return false;
+}
+
 void inizializza(List<Automobile> magazzino) {
     magazzino.add(new Automobile("toyota", "corolla", "AA000AA", Color.gray, 20000, LocalDate.now()));
     magazzino.add(new Automobile("ford", "focus", "GH092AJ", Color.blue, 350000, LocalDate.of(2008, 12, 3)));
@@ -110,9 +180,15 @@ void main() {
     ottieniImg.put("viola", new ImageIcon("img/macchina_viola.png"));
     ottieniImg.put("giallo", new ImageIcon("img/macchina_gialla.png"));
 
+    Map<Color, ImageIcon> colorToImageIcon = new HashMap<>();
+    colorToImageIcon.put(Color.red, new ImageIcon("img/macchina_rossa.png"));
+    colorToImageIcon.put(Color.blue, new ImageIcon("img/macchina_blu.png"));
+    colorToImageIcon.put(Color.green, new ImageIcon("img/macchina_verde.png"));
+    colorToImageIcon.put(Color.magenta, new ImageIcon("img/macchina_viola.png"));
+    colorToImageIcon.put(Color.yellow, new ImageIcon("img/macchina_gialla.png"));
+
     JFrame frame = new JFrame("Automobile");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(300, 200);
     frame.setLayout(null);
     frame.setSize(new Dimension(1500, 900));
     frame.setLocationRelativeTo(null);
@@ -123,27 +199,27 @@ void main() {
     scriviTarga.setBounds(0, 50, 250, 50);
 
     JLabel labelMarca = new JLabel("Inserisci la marca");
-    labelMarca.setBounds(275, 0, 250, 50);
+    labelMarca.setBounds(0, 110, 250, 50);
     JTextArea scriviMarca = new JTextArea();
-    scriviMarca.setBounds(275, 50, 250, 50);
+    scriviMarca.setBounds(0, 160, 250, 50);
 
     JLabel labelModello = new JLabel("Inserisci il modello");
-    labelModello.setBounds(550, 0, 250, 50);
+    labelModello.setBounds(0, 210, 250, 50);
     JTextArea scriviModello = new JTextArea();
-    scriviModello.setBounds(550, 50, 250, 50);
+    scriviModello.setBounds(0, 260, 250, 50);
 
-    JLabel labelKm = new JLabel("Inserisci il kilometraggio (non obbligatorio)");
-    labelKm.setBounds(825, 0, 250, 50);
+    JLabel labelKm = new JLabel("Inserisci il kilometraggio");
+    labelKm.setBounds(0, 310, 250, 50);
     SpinnerNumberModel modelKm = new SpinnerNumberModel(0, 0, null, 1000);
     JSpinner spinnerKm = new JSpinner(modelKm);
-    spinnerKm.setBounds(825, 50, 250, 50);
+    spinnerKm.setBounds(0, 360, 250, 50);
 
     JLabel mostraErrore = new JLabel();
     mostraErrore.setVerticalAlignment(SwingConstants.TOP);
     mostraErrore.setBounds(0, 430, 1000, 200);
 
-    JLabel labelImg = new JLabel();
-    labelImg.setBounds(400, 120, 500, 250);
+    JLabel labelImg = new JLabel(new ImageIcon("img/ruzza.jpg"));
+    labelImg.setBounds(500, 400, 500, 250);
 
     frame.add(labelTarga);
     frame.add(labelMarca);
@@ -157,18 +233,18 @@ void main() {
     frame.add(labelImg);
 
     JLabel labelColore = new JLabel("Scegli il colore");
-    labelColore.setBounds(275, 110, 150, 50);
+    labelColore.setBounds(550, 0, 250, 50);
     JRadioButton r1 = new JRadioButton("Rosso");
     JRadioButton r2 = new JRadioButton("Blu");
     JRadioButton r3 = new JRadioButton("Verde");
     JRadioButton r4 = new JRadioButton("Viola");
     JRadioButton r5 = new JRadioButton("Giallo");
 
-    r1.setBounds(275, 160, 100, 20);
-    r2.setBounds(275, 190, 100, 20);
-    r3.setBounds(275, 220, 100, 20);
-    r4.setBounds(275, 250, 100, 20);
-    r5.setBounds(275, 280, 100, 20);
+    r1.setBounds(550, 40, 150, 50);
+    r2.setBounds(550, 80, 150, 50);
+    r3.setBounds(550, 120, 150, 50);
+    r4.setBounds(550, 160, 150, 50);
+    r5.setBounds(550, 200, 150, 50);
     r1.setActionCommand("rosso");
     r2.setActionCommand("blu");
     r3.setActionCommand("verde");
@@ -200,22 +276,22 @@ void main() {
     frame.add(r5);
 
     JLabel labelGiorno = new JLabel("Inserisci il giorno di acquisto");
-    labelGiorno.setBounds(0, 110, 250, 50);
+    labelGiorno.setBounds(275, 0, 250, 50);
     SpinnerNumberModel modelGiorno = new SpinnerNumberModel(LocalDate.now().getDayOfMonth(), 1, 31, 1);
     JSpinner spinnerGiorno = new JSpinner(modelGiorno);
-    spinnerGiorno.setBounds(0, 160, 250, 50);
+    spinnerGiorno.setBounds(275, 50, 250, 50);
 
     JLabel labelMese = new JLabel("Inserisci il mese di acquisto");
-    labelMese.setBounds(0, 210, 250, 50);
+    labelMese.setBounds(275, 110, 250, 50);
     SpinnerNumberModel modelMese = new SpinnerNumberModel(LocalDate.now().getMonthValue(), 1, 12, 1);
     JSpinner spinnerMese = new JSpinner(modelMese);
-    spinnerMese.setBounds(0, 260, 250, 50);
+    spinnerMese.setBounds(275, 160, 250, 50);
 
     JLabel labelAnno = new JLabel("Inserisci l'anno di acquisto");
-    labelAnno.setBounds(0, 310, 250, 50);
+    labelAnno.setBounds(275, 210, 250, 50);
     SpinnerNumberModel modelAnno = new SpinnerNumberModel(LocalDate.now().getYear(), 1800, LocalDate.now().getYear(), 1);
     JSpinner spinnerAnno = new JSpinner(modelAnno);
-    spinnerAnno.setBounds(0, 360, 250, 50);
+    spinnerAnno.setBounds(275, 260, 250, 50);
 
     frame.add(labelGiorno);
     frame.add(labelMese);
@@ -224,38 +300,179 @@ void main() {
     frame.add(spinnerMese);
     frame.add(spinnerAnno);
 
+    JLabel labelScelta = new JLabel("Scegli cosa fare");
+    labelScelta.setBounds(700, 0, 250, 50);
+
+    JRadioButton nuova = new JRadioButton("Nuova");
+    nuova.setSelected(true);
+    JRadioButton visualizza = new JRadioButton("Visualizza");
+    JRadioButton modifica = new JRadioButton("Modifica");
+    JRadioButton elimina = new JRadioButton("Elimina");
+
+    nuova.setBounds(700, 40, 150, 50);
+    visualizza.setBounds(700, 80, 150, 50);
+    modifica.setBounds(700, 120, 150, 50);
+    elimina.setBounds(700, 160, 150, 50);
+
+    ButtonGroup opzioni = new ButtonGroup();
+    opzioni.add(nuova);
+    opzioni.add(visualizza);
+    opzioni.add(modifica);
+    opzioni.add(elimina);
+
+    nuova.setActionCommand("nuova");
+    visualizza.setActionCommand("visualizza");
+    modifica.setActionCommand("modifica");
+    elimina.setActionCommand("elimina");
+
+    frame.add(labelScelta);
+    frame.add(nuova);
+    frame.add(visualizza);
+    frame.add(modifica);
+    frame.add(elimina);
+
+    nuova.addActionListener(e -> {
+        scriviMarca.setEnabled(true);
+        scriviModello.setEnabled(true);
+        spinnerKm.setEnabled(true);
+        spinnerGiorno.setEnabled(true);
+        spinnerMese.setEnabled(true);
+        spinnerAnno.setEnabled(true);
+        r1.setEnabled(true);
+        r2.setEnabled(true);
+        r3.setEnabled(true);
+        r4.setEnabled(true);
+        r5.setEnabled(true);
+        scriviMarca.setText(null);
+        scriviModello.setText(null);
+        spinnerKm.setValue(0);
+        spinnerGiorno.setValue(LocalDate.now().getDayOfMonth());
+        spinnerMese.setValue(LocalDate.now().getMonthValue());
+        spinnerAnno.setValue(LocalDate.now().getYear());
+        group.clearSelection();
+        labelImg.setIcon(null);
+    });
+
+    visualizza.addActionListener(e -> {
+        scriviMarca.setEnabled(false);
+        scriviModello.setEnabled(false);
+        spinnerKm.setEnabled(false);
+        spinnerGiorno.setEnabled(false);
+        spinnerMese.setEnabled(false);
+        spinnerAnno.setEnabled(false);
+        r1.setEnabled(false);
+        r2.setEnabled(false);
+        r3.setEnabled(false);
+        r4.setEnabled(false);
+        r5.setEnabled(false);
+        scriviMarca.setText(null);
+        scriviModello.setText(null);
+        spinnerKm.setValue(0);
+        spinnerGiorno.setValue(LocalDate.now().getDayOfMonth());
+        spinnerMese.setValue(LocalDate.now().getMonthValue());
+        spinnerAnno.setValue(LocalDate.now().getYear());
+        group.clearSelection();
+        labelImg.setIcon(null);
+    });
+
+    modifica.addActionListener(e -> {
+        scriviMarca.setEnabled(false);
+        scriviModello.setEnabled(false);
+        spinnerKm.setEnabled(true);
+        spinnerGiorno.setEnabled(false);
+        spinnerMese.setEnabled(false);
+        spinnerAnno.setEnabled(false);
+        r1.setEnabled(true);
+        r2.setEnabled(true);
+        r3.setEnabled(true);
+        r4.setEnabled(true);
+        r5.setEnabled(true);
+        scriviMarca.setText(null);
+        scriviModello.setText(null);
+        spinnerKm.setValue(0);
+        spinnerGiorno.setValue(LocalDate.now().getDayOfMonth());
+        spinnerMese.setValue(LocalDate.now().getMonthValue());
+        spinnerAnno.setValue(LocalDate.now().getYear());
+        group.clearSelection();
+        labelImg.setIcon(null);
+    });
+
+    elimina.addActionListener(e -> {
+        scriviMarca.setEnabled(false);
+        scriviModello.setEnabled(false);
+        spinnerKm.setEnabled(false);
+        spinnerGiorno.setEnabled(false);
+        spinnerMese.setEnabled(false);
+        spinnerAnno.setEnabled(false);
+        r1.setEnabled(false);
+        r2.setEnabled(false);
+        r3.setEnabled(false);
+        r4.setEnabled(false);
+        r5.setEnabled(false);
+        scriviMarca.setText(null);
+        scriviModello.setText(null);
+        spinnerKm.setValue(0);
+        spinnerGiorno.setValue(LocalDate.now().getDayOfMonth());
+        spinnerMese.setValue(LocalDate.now().getMonthValue());
+        spinnerAnno.setValue(LocalDate.now().getYear());
+        group.clearSelection();
+        labelImg.setIcon(null);
+    });
+
     JButton conferma = new JButton("Conferma");
-    conferma.setBounds(275, 320, 100, 40);
+    conferma.setBounds(550, 260, 100, 40);
 
     conferma.addActionListener(e -> {
-        LocalDate data = null;
-        Color colore = null;
-        boolean errore = false;
-        try {
-            String valore = group.getSelection().getActionCommand();
-            colore = ottieniColore.get(valore);
-            data = LocalDate.of((int) spinnerAnno.getValue(), (int) spinnerMese.getValue(), (int) spinnerGiorno.getValue());
-        }
-        catch (Exception exc) {
-            mostraErrore.setText("Compila i campi correttamente");
-            errore = true;
+        switch (opzioni.getSelection().getActionCommand()) {
+            case "nuova":
+                LocalDate data = null;
+                Color colore = null;
+                boolean errore = false;
+                try {
+                    String valore = group.getSelection().getActionCommand();
+                    colore = ottieniColore.get(valore);
+                    data = LocalDate.of((int) spinnerAnno.getValue(), (int) spinnerMese.getValue(), (int) spinnerGiorno.getValue());
+                }
+                catch (Exception exc) {
+                    mostraErrore.setText("Compila i campi correttamente");
+                    errore = true;
+                }
+
+                if (!errore) {
+                    boolean corretto = create(magazzino, scriviMarca.getText(), scriviModello.getText(), scriviTarga.getText(), (int)spinnerKm.getValue(), colore, data, mostraErrore);
+                    if (corretto) {
+                        scriviTarga.setText(null);
+                        scriviMarca.setText(null);
+                        scriviModello.setText(null);
+                        spinnerKm.setValue(0);
+                        spinnerGiorno.setValue(LocalDate.now().getDayOfMonth());
+                        spinnerMese.setValue(LocalDate.now().getMonthValue());
+                        spinnerAnno.setValue(LocalDate.now().getYear());
+                        group.clearSelection();
+                        labelImg.setIcon(null);
+                        mostraErrore.setText("Macchina aggiunta correttamente");
+                    }
+                }
+                break;
+
+            case "visualizza":
+                int ind = read(magazzino, scriviTarga.getText().trim().toUpperCase(), mostraErrore);
+                if (ind != -1) {
+                    Automobile a = magazzino.get(ind);
+                    scriviMarca.setText(a.getMarca());
+                    scriviModello.setText(a.getModello());
+                    spinnerKm.setValue(a.getKm());
+                    spinnerGiorno.setValue(a.getDataDiFabbrica().getDayOfMonth());
+                    spinnerMese.setValue(a.getDataDiFabbrica().getMonthValue());
+                    spinnerAnno.setValue(a.getDataDiFabbrica().getYear());
+                    labelImg.setIcon(colorToImageIcon.get(a.getColore()));
+                }
+                break;
+
+            case "modifica":
+                break;
         }
 
-        if (!errore) {
-            boolean corretto = create(magazzino, scriviMarca.getText(), scriviModello.getText(), scriviTarga.getText(), (int)spinnerKm.getValue(), colore, data, mostraErrore);
-            if (corretto) {
-                scriviTarga.setText(null);
-                scriviMarca.setText(null);
-                scriviModello.setText(null);
-                spinnerKm.setValue(0);
-                spinnerGiorno.setValue(LocalDate.now().getDayOfMonth());
-                spinnerMese.setValue(LocalDate.now().getMonthValue());
-                spinnerAnno.setValue(LocalDate.now().getYear());
-                group.clearSelection();
-                labelImg.setIcon(null);
-                mostraErrore.setText("Macchina aggiunta correttamente");
-            }
-        }
     });
 
     frame.add(conferma);
